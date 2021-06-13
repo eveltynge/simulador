747c3762f0d4ed31b3be4f104692969967fd64a0
unknown <evelynge744@gmail.com>
2021-06-13 19:58:41 -0300

    Utilizacion de array

--- js/index.js
+++ js/index.js
@@ -1,74 +1,52 @@
- let nombre = prompt ("Por favor ingrese su nombre");
-        if (nombre == "") {
-            alert ("Por favor ingrese su nombre para continuar");
-        } else {
-        alert ("Hola " + nombre + ". " + "Bienvenido a tu simulador de préstamos"); 
-}
-/* const listaNombres = [];
-let   cantidad     = 5;
-//Empleo de do...while para cargar nombres en el array por prompt()
-do{
-    let nombre = prompt ("Por favor ingrese su nombre");
-   listaNombres.push(nombre.toUpperCase());
-   console.log(listaNombres.length);
-}while(listaNombres.length != cantidad) */
-
-
 class Prestamo {
-    constructor (monto, meses, interes) {
+    constructor (monto, meses) {
         this.monto = monto;
         this.meses = meses;
-        this.interes = interes;
+        switch (this.meses) {
+            case 12:
+                this.interes = 0.20;
+                break;
+            case 24:
+                this.interes = 0.30;
+                break;
+            case 36:
+                this.interes = 0.40;
+                break;
+        }
     }
     montoFinal(){
         this.montoTotal = this.monto + this.monto*this.interes;
+        return this.montoTotal;
     }
     cuotaFinal(){
         this.cuota = this.montoTotal / this.meses;
+        return this.cuota;
     }
 }
-monto = 0;
-do {
-    monto = (parseInt(prompt("Ingrese un monto mayor a 0 para simular su préstamo")));
-} while (monto <= 0);
-meses = 0;
-do {
-    meses = (parseInt(prompt("Seleccione entre 12, 24 o 36 meses")));
-} while ((meses != 12) && (meses != 24) && (meses != 36));
 
-
-/* monto = (parseInt(prompt("Ingrese el monto a simular")));
-if (monto <= 0) {
-    alert("El monto ingresado no puede ser menor a 0");
-}else {
-    meses = (parseInt(prompt("Seleccione entre 12, 24 o 36 meses")));
-    if ((meses == 12) || (meses == 24) || (meses == 36)) {
-        alert("Ud ingresó " + monto + " " + "en" + " " + meses + " meses");
-    }else {
-        alert("La cantidad de meses seleccionados no está disponible");
+const solicitudes = [];
+let cantidad = 3;
+while (solicitudes.length < cantidad) {
+    let nombre = prompt ("Por favor ingrese su nombre");
+    while (nombre == ""){
+        alert ("Por favor ingrese su nombre para continuar");
     }
-} */
-/* switch (meses) {
-    case 12:
-        alert ("Ud ingresó " + monto + " " + "en" + " " + meses + " meses");
-        break;
-    case 24:
-        alert ("Ud ingresó " + monto + " " + "en" + " " + meses + " meses");
-        break;
-    case 36:
-        alert ("Ud ingresó " + monto + " " + "en" + " " + meses + " meses");
-        break;
-    default:
-        alert ("La cantidad de meses seleccionados no está disponible")
-} */
-const prestamo1 = new Prestamo (monto, 12, 0.20);
-const prestamo2 = new Prestamo (monto, 24, 0.30);
-const prestamo3 = new Prestamo (monto, 36, 0.40);
+    alert ("Hola " + nombre + ". " + "Bienvenido a tu simulador de préstamos");
+    solicitudes.push(nombre);
+    monto = 0;
+    do {
+        monto = (parseInt(prompt("Ingrese un monto mayor a 0 para simular su préstamo")));
+    } while (monto <= 0);
+    meses = 0;
+    do {
+        meses = (parseInt(prompt("Seleccione entre 12, 24 o 36 meses")));
+    } while ((meses != 12) && (meses != 24) && (meses != 36));
 
-console.log(prestamo1);
-prestamo1.montoFinal();
-prestamo1.cuotaFinal();
-
+    const prestamo1 = new Prestamo (monto, meses);
+    alert ("Ud ingresó " + prestamo1.monto + " en " + prestamo1.meses + " meses");
+    alert ("Ud abonará "+ prestamo1.montoFinal() + " en cuotas de " + prestamo1.cuotaFinal() + " con un interes de " + prestamo1.interes);
+}
+console.log(solicitudes);
 
 
 
