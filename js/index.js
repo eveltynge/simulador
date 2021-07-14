a4de4d27a9199881c4c394b0751b804230e8ade7
unknown <evelynge744@gmail.com>
2021-07-14 19:09:21 -0300

    nueva version de tp, con cambios inconclusos de css. uso de ajax

--- js/index.js
+++ js/index.js
@@ -1,72 +1,65 @@
+const tasa = 0.0125;
+const iva = 0.21;
+
 const solicitantes = [];
 
-class Solicitud {
-    constructor(nombre, apellido, monto, meses){
+class Prestamo {
+    constructor (nombre, apellido, monto, plazo){
         this.nombre=nombre,
         this.apellido=apellido,
-        this.monto=monto;
-        this.meses=meses;
-        this.montoTotal = 0;
-        this.cuota = 0;
-        this.interes = 0;
-        switch (this.meses){
-            case 12:
-                this.interes = 0.20;
-                break;
-            case 24:
-                this.interes = 0.30;
-                break;
-            case 36:
-                this.interes = 0.40;
-                break;
-        }
+        this.monto=monto,
+        this.plazo=plazo,
+        this.pagar = 0;
+        this.cuotaUno = 0;
     }
-    montoFinal(){
-        this.montoTotal = this.monto + this.monto*this.interes;
-        return this.montoTotal;
+    pagarFinal(){
+        this.pagar = this.monto + this.monto * iva + this.monto * tasa;
+        return this.pagar;
     }
-    cuotaFinal(){
-        this.cuota = this.montoTotal / this.meses;
-        return this.cuota;
+    cuotaInicial(){
+        this.cuotaUno = this.pagar / this.plazo;
+        return this.cuotaUno;
     }
 }
-
-$('section').prepend('<button id="btn" type="submit" class="btn btn-success" value="Simular">Simular</button>');
-$('#btn').on('click', function Simular(e) {
-    e.preventDefault();    
+$('.calculator').append('<button id="btn" type="submit" class="btn btn-success col-2" value="Simular" style="margin: 15px 0px;">Simular</button>');
+$('#btn').on('click', function Simular(e){
+    e.preventDefault(); 
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     let monto = parseInt(document.getElementById("mntSlc").value);
-    let mesesSelec = document.querySelector('input[name="cuotas"]:checked');
+    let mesesSelec = document.querySelector('option[name="cuotas"]:checked');
     let meses = parseInt(mesesSelec.value);
-        
-    let solicitantesObj = new Solicitud(nombre, apellido, monto, meses);
-    solicitantesObj.montoFinal();
-    solicitantesObj.cuotaFinal();
-    solicitantes.push(solicitantesObj);
+
+    let prestamoObj = new Prestamo(nombre, apellido, monto, meses);
+    prestamoObj.pagarFinal();
+    prestamoObj.cuotaInicial();
+    solicitantes.push(prestamoObj);
     console.log(solicitantes);
-    
-    $('.contenedor').append(`
-    <div class="contenedor border border-success" style="padding: 3px"><p>Usuario: ${solicitantesObj.nombre} ${solicitantesObj.apellido}</p>
-    <p>Monto Solicitado: ${solicitantesObj.monto}</p>
-    <p>Cuota: ${solicitantesObj.meses} </p>
-    <p>Monto a devolver: ${solicitantesObj.montoTotal} </p>
-    <p>Valor cuota: ${solicitantesObj.cuota.toFixed(2)} </p>
+
+    $('.contenedorSolicitantes').append(`
+    <div id="datos" class="btnForm border border-success" style="padding: 3px">
+    <p>Usuario: ${prestamoObj.nombre} ${prestamoObj.apellido}</p>
+    <p>Monto Solicitado: ${prestamoObj.monto}</p>
+    <p>Plazo en meses: ${prestamoObj.plazo} </p>
+    <p>Monto a devolver: ${prestamoObj.pagar} </p>
+    <p>Valor primera cuota: ${prestamoObj.cuotaUno.toFixed(2)} </p>
     </div>`)
-    
-const guardados = JSON.stringify(solicitantes);
-localStorage.setItem("solicitantes", guardados);
+
+    const guardados = JSON.stringify(solicitantes);
+    localStorage.setItem("solicitantes", guardados);
 });
 
 solicitantes.forEach(solicitante => {
     removeAllChildNodes(div);
-    $('.contenedor').append(`
-    <div><p>Usuario: ${solicitante.nombre} ${solicitante.apellido}</p>
+    $('.contenedor').append( `
+    <div class="btnForm"><p>Usuario: ${solicitante.nombre} ${solicitante.apellido}</p>
     <p>Monto Solicitado: ${solicitante.monto}</p>
-    <p>Cuota: ${solicitante.meses} </p>
-    <p>Monto a devolver: ${solicitante.montoTotal} </p>
-    <p>Valor cuota: ${solicitante.cuota.toFixed(2)} </p>
-    </div>`)
+    <p>Cuota: ${solicitante.plazo} </p>
+    <p>Monto a devolver: ${solicitante.pagar} </p>
+    <p>Valor cuota: ${solicitante.cuotaUno.toFixed(2)} </p>
+    </div>`);
+
+    section.appendChild(div);
 })
 
 function removeAllChildNodes(parent) {
@@ -76,3 +69,15 @@ function removeAllChildNodes(parent) {
 }
 const guardados = JSON.stringify(solicitantes);
 localStorage.setItem("solicitantes", guardados);
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+$('.contenedorSolicitantes').after('<button id="btnDatos" type="submit" class="btn btn-success" value="Simular" style="margin:10px">Guardar Solicitud</button>');
+$("#btnDatos").click(() => { 
+    $.post(url, solicitantes,(respuesta, estado) => {
+        if(estado === "success"){
+            $('.btnForm').after(`<div>
+            Su solicitud quedó guardada con el id:${respuesta.id}
+            </div>`);
+        }  
+    });
+});
