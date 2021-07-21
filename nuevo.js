ae46e2bd8a3a852af061f4e29bbdd763e3f258bc
unknown <evelynge744@gmail.com>
2021-07-21 18:32:51 -0300

    agregue modal al html. corregi append para que funcione en modal, a revisar. uso de localstorage para botones

--- nuevo.js
+++ nuevo.js
@@ -4,13 +4,20 @@ const iva = 0.21;
 const solicitantes = [];
 
 class Prestamo {
-    constructor (nombre, apellido, monto, plazo){
+    constructor (nombre, apellido, monto, plazo, dni, mail){
         this.nombre=nombre,
         this.apellido=apellido,
         this.monto=monto,
         this.plazo=plazo,
+        this.dni=dni,
+        this.mail=mail,
+        this.id = 1;
         this.pagar = 0;
         this.cuotaUno = 0;
+    }
+    nroId(){
+      this.id = this.id + 1;
+      return this.id;
     }
     pagarFinal(){
         this.pagar = this.monto + this.monto * iva + this.monto * tasa;
@@ -21,7 +28,7 @@ class Prestamo {
         return this.cuotaUno;
     }
 }
-$('.calculator').append('<button id="btn" type="button" data-toggle="modal" data-target="#staticBackdrop" class="btn btn-success col-2" value="Simular" style="margin: 15px 0px;">Simular</button>');
+
 $('#btn').on('click', function Simular(e){
   e.preventDefault(); 
   let nombre = document.getElementById("nombre").value;
@@ -29,15 +36,18 @@ $('#btn').on('click', function Simular(e){
   let monto = parseInt(document.getElementById("mntSlc").value);
   let mesesSelec = document.querySelector('option[name="cuotas"]:checked');
   let meses = parseInt(mesesSelec.value);
+  let dni = document.getElementById("dni").value;
+  let mail = document.getElementById("mail").value;
+  let situacion = document.querySelector('option[name="situacion"]:checked');
 
-  let prestamoObj = new Prestamo(nombre, apellido, monto, meses);
+  let prestamoObj = new Prestamo(nombre, apellido, monto, meses, dni, mail, situacion);
   prestamoObj.pagarFinal();
   prestamoObj.cuotaInicial();
   solicitantes.push(prestamoObj);
   console.log(solicitantes);
 
   const url = "https://jsonplaceholder.typicode.com/posts";
-  
+
   $('.modal-body').append(`
     <div id="datos" class="modal-body">
         ${$.post(url, solicitantes,(respuesta, estado) => {
@@ -50,29 +60,25 @@ $('#btn').on('click', function Simular(e){
       })}                
     </div>`)
 
-  const guardados = JSON.stringify(solicitantes);
-  localStorage.setItem("solicitantes", guardados);
-});
-
-$('.btnDatos').click(function(){
-    $('.modal-body').empty();
-  })
-  
 const guardados = JSON.stringify(solicitantes);
 localStorage.setItem("solicitantes", guardados);
+});
 
-/* const url = "https://jsonplaceholder.typicode.com/posts";
-//$('.contenedorSolicitantes').after('<button id="btnDatos" type="submit" class="btn btn-success" value="Simular" style="margin:10px">Guardar Solicitud</button>');
-$("#btnDatos").click(() => {    
-    $.post(url, solicitantes,(respuesta, estado) => {
-        if(estado === "success"){
-            $('.btnForm').after(`<div>
-            Su solicitud quedó guardada con el id:${respuesta.id}
-            En la brevedad nos contactaremos con Ud para completar la solicitud del préstamo.
-            </div>`);
-        }  
-    });
-    
-}); */
+//ESTO FUNCIONA OK!!!
+$('.btnDatos').click(function(){
+  $('.modal-body').empty();
+})
+//Por que necesito que este dos veces??
+const guardados = JSON.stringify(solicitantes);
+localStorage.setItem("solicitantes", guardados);
+//Borra Todo otra vez
+$('#btnC').click(function(){
+  localStorage.removeItem("solicitantes", guardados);
+})
 
-
+//GUARDAR INFO SOLICITANTES AL APRETAR BOTON GUARDAR
+/* const guardados = JSON.stringify(solicitantes);
+console.log(guardados);
+$('#btnG').click(function(){
+  localStorage.setItem("solicitantes", guardados)
+}) */
